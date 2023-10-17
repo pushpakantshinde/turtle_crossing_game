3cfab7a96887aa1c6dfe991a787c5d47dd91a678
Pushpakant Shindeclc <pushpakantshinde@gmail.com>
2023-10-17 21:33:58 +0530

    main.py updated

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from turtle import Screen, Turtle
+import time
 
+my_screen = Screen()
+my_screen.setup(width=600, height=600)
+my_screen.bgcolor("white")
+my_screen.title("Turtle-crossing Game")
+my_screen.tracer(0)
+
+
+my_screen.exitonclick()
