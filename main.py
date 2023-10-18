a3f825f83ee730968866a7326225a06dc0f832b7
Pushpakant Shindeclc <pushpakantshinde@gmail.com>
2023-10-18 22:10:08 +0530

    main.py updated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 from player import Player
+from cars import Cars
 import time
 
 my_screen = Screen()
@@ -9,6 +10,7 @@ my_screen.title("Turtle-crossing Game")
 my_screen.tracer(0)
 
 player = Player()
+car_manager = Cars()
 
 my_screen.listen()
 my_screen.onkey(player.move_player, "Up")
@@ -16,7 +18,24 @@ my_screen.onkey(player.move_player, "Up")
 is_game_on = True
 
 while is_game_on:
-    pass
+    time.sleep(0.1)
+    my_screen.update()
+
+    car_manager.create_cars()
+    car_manager.move_car
+
+    # Detect when turtle reaches to other side
+    if player.ycor() > 280:
+        player.reset_position()
+        car_manager.level_up()
+
+    # Detect collision with the car
+    for car in car_manager.cars_list:
+        if player.distance(car) < 20:
+            player.clear_player()           # Clear the player on screen
+            car_manager.clear_cars()        # Clear the cars on the screen
+            my_screen.update()              # Need to update the screen otherwise we cant see clearing of player & cars
+            is_game_on = False
 
 
 my_screen.exitonclick()
