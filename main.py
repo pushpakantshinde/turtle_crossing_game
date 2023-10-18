c69db8af059428a97f8c6c515994ec23f035c6f6
Pushpakant Shindeclc <pushpakantshinde@gmail.com>
2023-10-18 21:50:29 +0530

    main.py updated

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from player import Player
 import time
 
 my_screen = Screen()
@@ -6,6 +7,16 @@ my_screen.setup(width=600, height=600)
 my_screen.bgcolor("white")
 my_screen.title("Turtle-crossing Game")
 my_screen.tracer(0)
+
+player = Player()
+
+my_screen.listen()
+my_screen.onkey(player.move_player, "Up")
+
+is_game_on = True
+
+while is_game_on:
+    pass
 
 
 my_screen.exitonclick()
